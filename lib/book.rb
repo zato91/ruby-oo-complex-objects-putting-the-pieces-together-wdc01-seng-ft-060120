ecac4febd77a9f2cae3a3c12a66db671c244084c
zara ahmadi <zatokey@gmail.com>
2020-06-04 14:09:36 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,28 @@
-
+class Book
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def title
+    @title
+  end
+ 
+  def author=(author)
+    @author = author
+  end
+ 
+  def author
+    @author
+  end
+ 
+  def page_count=(num)
+    @page_count = num
+  end
+ 
+  def page_count
+    @page_count
+  end
+ 
+end
 
