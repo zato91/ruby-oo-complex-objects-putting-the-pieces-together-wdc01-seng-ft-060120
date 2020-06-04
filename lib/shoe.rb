b5a18367269e2f3350530672826ae1d23dda4309
zara ahmadi <zatokey@gmail.com>
2020-06-04 14:13:20 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -17,4 +17,20 @@ def color
   @color
 end 
 
+def color=(color)
+  @color = color
+end 
+
+def color
+  @color
+end 
+
+def color=(color)
+  @color = color
+end 
+
+def color
+  @color
+end 
+
 end 
